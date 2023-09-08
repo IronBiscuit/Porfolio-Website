7bf8179ad1d11aa6284fd54958710c91d862c037
Lim Yu Yang <limyuyang.98@gmail.com>
2023-09-08 17:20:07 +0800

    Add Official Education Timeline

--- public/JS/rootJQuery.js
+++ public/JS/rootJQuery.js
@@ -1,16 +1,6 @@
 (function ($) {
     "use strict";
 
-    // Navbar on scrolling
-    // $(window).scroll(function () {
-    //     if ($(this).scrollTop() > 200) {
-    //         $('.navbar').fadeIn('slow').css('display', 'flex');
-    //     } else {
-    //         $('.navbar').fadeOut('slow').css('display', 'none');
-    //     }
-    // });
-
-
     //Smooth scrolling on the navbar links
     $(".nav a").on('click', function (event) {
         if (this.hash !== "") {
@@ -63,34 +53,6 @@
             loop: true
         });
     }
-
-
-    // Modal Video
-    // $(document).ready(function () {
-    //     var $videoSrc;
-    //     $('.btn-play').click(function () {
-    //         $videoSrc = $(this).data("src");
-    //     });
-    //     console.log($videoSrc);
-
-    //     $('#videoModal').on('shown.bs.modal', function (e) {
-    //         $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
-    //     })
-
-    //     $('#videoModal').on('hide.bs.modal', function (e) {
-    //         $("#video").attr('src', $videoSrc);
-    //     })
-    // });
-
-
-    // Scroll to Bottom
-    // $(window).scroll(function () {
-    //     if ($(this).scrollTop() > 100) {
-    //         $('.scroll-to-bottom').fadeOut('slow');
-    //     } else {
-    //         $('.scroll-to-bottom').fadeIn('slow');
-    //     }
-    // });
 
 
     // Skills
